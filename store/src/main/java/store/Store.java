f276ee90e7e116c3861b68a3509a5b84799ad6bb
LucaDLL <lucadelloglio@hotmail.it>
2020-05-06 13:14:31 +0200

    Receiving messages, replication first steps

--- store/src/main/java/store/Store.java
+++ store/src/main/java/store/Store.java
@@ -3,7 +3,6 @@ package store;
 import java.io.File;
 import java.util.Collections;
 
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.*;
 import akka.stream.ActorMaterializer;
@@ -18,26 +17,39 @@ import grpc.*;
 import java.util.concurrent.CompletionStage;
 
 class Store {
+
+  final static Config config = ConfigFactory.parseFile(new File("conf/application.conf"));
+
   public static void main(String[] args) throws Exception {
 
     try {
-      if(args[0].equals("init") && args.length == 1) {
-        //Enable HTTP/2 in ActorSystem's config
-        Config conf = ConfigFactory.parseString("akka.http.server.preview.enable-http2 = on")
-        .withFallback(ConfigFactory.defaultApplication());
-  
-        // Akka ActorSystem Boot
-        ActorSystem sys = ActorSystem.create("StoreSystem", conf);
-  
-        //gRPC server binding
+
+      if (args[0].equals("init") && args.length == 1) {
+
+        /*
+          Start system and seed nodes
+        */
+        ActorSystem sys = startSeedNodes();
+
+        /*
+          Parse replication factor from config
+        */
+        Integer r = Integer.parseInt(config.getString("replication.factor"));
+        
+        /*
+          To have R replicas, at least R cluster nodes are needed.
+          Two seed nodes have been already created, so R-2 more nodes are created.
+        */
+        for (int i = 0; i < r-3; i++) startNode(0);
+        
+        /*
+          gRPC server binding
+        */
         run(sys).thenAccept(binding -> {
-        System.out.println("gRPC server bound to: " + binding.localAddress());
+          System.out.println("gRPC server bound to: " + binding.localAddress());
         });
-  
-        startNode(25251);
-        startNode(25252);
-      }
-      else if(args[0].equals("add") && args.length == 2) {
+
+      } else if(args[0].equals("add") && args.length == 2) {
   
         try {
           Integer nodesNum = Integer.parseInt(args[1]);
@@ -45,18 +57,19 @@ class Store {
         } catch (NumberFormatException e) {
           throw new Exception();
         }
-      }
-      else throw new Exception();
+
+      } else throw new Exception();
+
     } catch (Exception e) {
-        System.out.println("Bad input");
+        e.printStackTrace();
     }
   }
 
-  public static CompletionStage<ServerBinding> run(ActorSystem sys) throws Exception {
+  private static CompletionStage<ServerBinding> run(ActorSystem sys) throws Exception {
     Materializer mat = ActorMaterializer.create(sys);
 
     // Instantiate implementation
-    StoreService impl = new StoreServiceImpl();
+    StoreService impl = new StoreServiceImpl(sys);
 
     return Http.get(sys).bindAndHandleAsync(
       StoreServiceHandlerFactory.create(impl, sys),
@@ -64,18 +77,27 @@ class Store {
       mat);
   }
 
-	public static ActorRef startNode(int port) {
-		// Override the configuration of the port
-		final Config config = ConfigFactory //
-			.parseFile(new File("conf/application.conf")) //
-			.withValue("akka.remote.artery.canonical.port", ConfigValueFactory.fromAnyRef(port))
-			.withValue("akka.cluster.roles", ConfigValueFactory.fromIterable(Collections.singletonList("storeNode")));
+  private static ActorSystem startSeedNodes() {
+
+      startNode(25251);
+      return startNode(25252);
+  
+  }
+
+	private static ActorSystem startNode(int port) {
+    
+    // Override the configuration of the port
+    final Config nodeConfig = config
+      .withValue("akka.remote.artery.canonical.port", ConfigValueFactory.fromAnyRef(port))
+      .withValue("akka.cluster.roles", ConfigValueFactory.fromIterable(Collections.singletonList("storeNode")));
 
 		// Create an Akka system
-		final ActorSystem system = ActorSystem.create("StoreSystem", config);
+		final ActorSystem system = ActorSystem.create("StoreSystem", nodeConfig);
 
 		// Create an actor
-		return system.actorOf(StoreNode.props(), "storeNode");
-	}
+    system.actorOf(StoreNode.props(), "storeNode");
+    
+    return system;
+  }
 
 }
--- store/src/main/java/store/StoreServiceImpl.java
+++ store/src/main/java/store/StoreServiceImpl.java
@@ -1,5 +1,11 @@
 package store;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.cluster.Cluster;
+import akka.cluster.Member;
+
+import messages.*;
 import grpc.*;
 
 import java.util.concurrent.CompletableFuture;
@@ -7,11 +13,23 @@ import java.util.concurrent.CompletionStage;
 
 public class StoreServiceImpl implements StoreService {
     
+    ActorSystem system;
+    Cluster cluster;
+
+    StoreServiceImpl(ActorSystem system) {
+        this.system = system;
+        this.cluster = Cluster.get(system);
+    }
+
     @Override
     public CompletionStage<PutReply> put(PutRequest in){
-        System.out.println("key " + in.getKey());
-        System.out.println("value " + in.getValue());
+        
+        Member member = (cluster.state().getMembers()).iterator().next();
+        String address = member.address().toString() + "/user/storeNode";
+        PutMsg msg = new PutMsg(in.getKey(), in.getValue());
 
+        system.actorSelection(address).tell(msg, ActorRef.noSender());
+        
         PutReply reply = PutReply.newBuilder().setMessage("Received put(key: " + in.getKey() + ", value: " + in.getValue() + ")").build();
         return CompletableFuture.completedFuture(reply);
     }
